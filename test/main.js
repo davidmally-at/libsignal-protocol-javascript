93264d97a11850026a07dc6e4f8f2d6cd50fccd3
elsehow <yes@cosmopol.is>
2016-12-11 22:04:58 -0800

    Passing store tests
    The store tests come in a set of 5.
    Each gets initialized as a function,
    and SignalProtocolStore_test.js ties them all together.
    
    Where before the methods were being called from out of scope,
    here we require each store test in test/main.js, then pass all of these functions into test/SiganlProtocolStore_test.js, which now exposes a higher-order function.

--- test/main.js
+++ test/main.js
@@ -2,9 +2,20 @@
 mocha.setup("bdd");
 window.assert = chai.assert;
 
-// require('./helpers_test.js');
-// require('./crypto_test.js');
-// require('./SessionCipherTest.js');
-// require('./KeyHelperTest.js');
-// require('./NumericFingerprintTest.js');
+require('./helpers_test.js');
+require('./crypto_test.js');
+require('./SessionCipherTest.js');
+require('./KeyHelperTest.js');
+require('./NumericFingerprintTest.js');
 require('./SessionBuilderTest.js');
+var testIdentityKeyStore = require('./IdentityKeyStore_test.js');
+var testPreKeyStore = require('./PreKeyStore_test.js');
+var testSignedPreKeyStore = require('./SignedPreKeyStore_test.js');
+var testSessionStore = require('./SessionStore_test.js');
+require('./SignalProtocolStore_test.js')(
+  testIdentityKeyStore,
+  testPreKeyStore,
+  testSignedPreKeyStore,
+  testSessionStore
+);
+// require('./SignalProtocolAddressTest.js');
