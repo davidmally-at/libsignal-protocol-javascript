f2eda0e59f9269bedc133e01f86f3b311c1f1539
elsehow <yes@cosmopol.is>
2016-12-12 02:53:16 -0800

    Added blanket; tests fail
    After adding blanket back, tests are suddenly failing!
    Not sure why.

--- src/curve25519_worker.js
+++ src/curve25519_worker.js
@@ -3,12 +3,9 @@ var CurveWrapper = require('./curve25519_wrapper2.js')
 
 module.exports = function (self) {
   self.onmessage = function(e) {
-    console.log('worker message!', e)
     CurveWrapper.curve25519_async[e.data.methodName].apply(null, e.data.args).then(function(result) {
-      console.log('worker result!!!', result)
       self.postMessage({ id: e.data.id, result: result });
     }).catch(function(error) {
-      console.log("worker err?", error)
       self.postMessage({ id: e.data.id, error: error.message });
     });
   };
